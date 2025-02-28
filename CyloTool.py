64c67497472175148a9b325561a398e84dba6383
Bubcyz <detorresmarc08@gmail.com>
2025-02-28 10:45:29 +0800

    Update CyloTool.py

--- CyloTool.py
+++ CyloTool.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from cylotool import CyloTool
+from bubcyz import Bubcyz
 
 __CHANNEL_USERNAME__ = "CyloToolChannel"
 __GROUP_USERNAME__   = "CyloToolChat"
